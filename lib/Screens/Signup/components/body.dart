4c082db804e51e5f85e041f6c2c06ed7791c615c
NCM <neenuchacko@live.in>
2020-12-18 23:05:37 +0530

    Scne anlo

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -8,8 +8,66 @@ import 'package:flutter_app/components/rounded_button.dart';
 import 'package:flutter_app/components/rounded_input_field.dart';
 import 'package:flutter_app/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
+import 'package:flutter_app/social_auth.dart';
+import 'package:flutter_app/profile.dart';
+import 'package:flutter_app/global.dart';
 
-class Body extends StatelessWidget {
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  bool _isLoggedIn = false;
+
+  final facebookLogin = FacebookLogin();
+
+  _loginWithFB() async {
+    final result = await facebookLogin.logIn(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userProfile = profile;
+          _isLoggedIn = true;
+        });
+
+        Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ProfileScreen();
+                    },
+                  ),
+                );
+     
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false);
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false);
+        break;
+    }
+  }
+
+  _logout() {
+    facebookLogin.logOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -18,11 +76,10 @@ class Body extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-                        SizedBox(height: size.height * 0.03),
-
+            SizedBox(height: size.height * 0.03),
             Text(
-              "SIGNUP",
-              style: TextStyle(fontWeight: FontWeight.bold,fontSize:16),
+              'SIGNUP',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
             ),
             SizedBox(height: size.height * 0.03),
             SvgPicture.asset(
@@ -60,7 +117,12 @@ class Body extends StatelessWidget {
               children: <Widget>[
                 SocalIcon(
                   iconSrc: "assets/icons/facebook.svg",
-                  press: () {},
+                  press: () {
+                    _loginWithFB();
+
+                    
+                    
+                  },
                 ),
                 SocalIcon(
                   iconSrc: "assets/icons/twitter.svg",
@@ -68,11 +130,140 @@ class Body extends StatelessWidget {
                 ),
                 SocalIcon(
                   iconSrc: "assets/icons/google-plus.svg",
-                  press: () {},
+                  press: () async{
+                    await signInGoogle();
+
+                  },
                 ),
               ],
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+/*class ProfileScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(userProfile["name"]),
+    );
+  }
+}*/
+class ProfileScreen extends StatelessWidget {
+  
+  void _showDialog(BuildContext context, {String title, String msg}) {
+    final dialog = AlertDialog(
+      title: Text(title),
+      content: Text(msg),
+      actions: <Widget>[
+        RaisedButton(
+          color: Colors.teal,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Close',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+    showDialog(context: context, builder: (x) => dialog);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget>[
+            
+            Image.network(userProfile["picture"]["data"]["url"], height: 100.0, width: 100.0,),
+            
+            
+                
+            Text(userProfile["name"]),
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: Divider(
+                color: Colors.teal.shade700,
+              ),
+            ),
+            
+            InfoCard(
+              text: userProfile['email'],
+              icon: Icons.email,
+              onPressed: () {},
+            ),
+            /*InfoCard(
+              text: 'than aaruva?',
+              icon: Icons.web,
+              onPressed: () {},
+            ),
+            InfoCard(
+              text: 'enthelum venel ida',
+              icon: Icons.location_city,
+              onPressed: null,
+            ),*/
+
+            RaisedButton(
+              padding: EdgeInsets.all(8),
+              color: Colors.blueGrey,
+              child: const Text('Sign Out', style: TextStyle( color: Colors.white)),
+              onPressed: (){
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => LoginScreen()));
+
+              
+
+            }
+            
+            )
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  Function onPressed;
+
+  InfoCard({
+    @required this.text,
+    @required this.icon,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Card(
+        color: Colors.white60,
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+        child: ListTile(
+          leading: Icon(
+            icon,
+            color: Colors.blueGrey,
+          ),
+          title: Text(
+            text,
+            //style: GoogleFonts.sourceSansPro(fontSize: 20, color: Colors.blueGrey),
+          ),
         ),
       ),
     );
---
+++ lib/social_auth.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+Future<UserCredential> signInGoogle() async{
+  await Firebase.initializeApp();
+  final GoogleSignInAccount user=await GoogleSignIn().signIn();
+  final GoogleSignInAuthentication auth= await user.authentication;
+  AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken:auth.accessToken,
+    idToken:auth.idToken,
+  );
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+  
+}
